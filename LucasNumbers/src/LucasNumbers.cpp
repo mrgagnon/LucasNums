91ebde93c3af5595264e9e000c81cde54caa4e3f
Maylee <mgagnon987@gmail.com>
2019-11-07 09:11:55 -0500

    q2 lucas numbers recursive seems to work

--- LucasNumbers/src/LucasNumbers.cpp
+++ LucasNumbers/src/LucasNumbers.cpp
@@ -7,7 +7,43 @@
 #include <iostream>
 using namespace std;
 
+// TODO printing
+
+/* Recursive function to calculate the Nth term of Lucas numbers
+ * @param n Nth term to be calculated
+ * @return value of the Nth term
+ */
+int lucas(int n) {
+	if (n == 0){ // hard code, initial condition L(0) = 2
+		return 2;
+	}
+	else if (n == 1) { // hard code, initial condition L(1) = 1
+		return 1;
+	}
+	return lucas(n-1) + lucas(n-2); //L(n-1)+L(n-2) for n>1
+}
+
+/* gathers user input,
+ * calls recursive function to calculate
+ * prints numbers
+ */
 int main() {
-	cout << "Hello World" << endl;
+	int num;
+	int i = 0;
+
+	cout << "Enter number of terms" << endl;
+	cin >> num;
+	if (cin.bad()) {
+		cout << "Bad input. Exiting." << endl;
+		return 0;
+	}
+
+	//cout << lucas(num);
+
+	while ( i <= num){
+		cout << lucas(i) << " "; // calling recursive lucas function to calculate & print values
+		i++;
+	}
+	cout << endl;
 	return 0;
 }
